eca0447ec6fb53f86c223f98e63d6770c5404c04
GESLucaBabetto <luca.babetto@greenenergystorage.eu>
2021-10-08 12:15:49 +0200

    v0.3.1 - included temporary "main" file template

---
+++ main.py
@@ -0,0 +1,36 @@
+# Group of cycles
+class CellCycling:
+
+    # --- FILE INPUT
+    # build cycle sequence from multiple chronopotentiometry files
+    def build_cycles:
+    pass
+
+
+
+    # read n files each containing n cycles
+    def read_cycling:
+    pass
+
+    # --- PROPERTIES
+    # property function decorator
+    def compute_cretention:
+    pass
+    # property function decorator
+    def compute_efficiencies:
+    pass
+
+
+
+# Single cycle
+class Cycle:
+
+    # property function decorator
+    def compute_efficiencies:
+    pass
+    # property function decorator
+    def compute_capacities:
+    pass
+    # property function decorator
+    def compute_total_energies:
+    pass
